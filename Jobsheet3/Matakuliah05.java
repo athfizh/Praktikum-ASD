197aa193c7f5514679ad8258bd885f740888aa66
athfizh <athaullahafizh@gmail.com>
2025-02-27 11:53:27 +0700

    percobaan

---
+++ Jobsheet3/Matakuliah05.java
@@ -0,0 +1,14 @@
+package Jobsheet3;
+public class Matakuliah05 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public Matakuliah05 (String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
